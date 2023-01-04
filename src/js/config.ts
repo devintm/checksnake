09954ef2f283d5d6c9b9c28fffdd93f9641b5c4f
Devin TM <devintmla@gmail.com>
2023-01-05 00:28:09 +0530

    Clean up after merge.

--- src/js/config.ts
+++ src/js/config.ts
@@ -1,24 +1,7 @@
-type coordinates = [number, number];
-
-type keycodes = {
-    arrowUp: number,
-    arrowDown: number,
-    arrowLeft: number,
-    arrowRight: number
-}
-
 const speed: number = 6;
-const worldSize: number = 10;
-// const startPoint = [5, 5] as [number, number];
-// const startPoint: [number, number] = [5, 5];
-const startPoint: coordinates = [5, 5];
-const snake: coordinates[] = [startPoint];
-const key = {
-    arrowUp: 38,
-    arrowDown: 40,
-    arrowLeft: 37,
-    arrowRight: 39
-};
+const worldSize: number = 12;
+const startPoint: [number, number] = [7, 7];
+const snake = [startPoint];
 
-let movingDirection;
-let moveInterval;
+let movingDirection: Direction;
+let moveInterval: ReturnType<typeof setTimeout>;
